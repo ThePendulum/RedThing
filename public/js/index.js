480af5b41580b3ac0a818392a06952f8a7486f8f
Niels Simenon <niels.simenon@gmail.com>
2022-05-23 01:48:14 +0200

    Added rudimentary playback controls.

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,16 @@ const collectionSize = document.querySelector('#collectionSize');
 const collectionContent = document.querySelector('#collectionContent');
 
 const player = document.querySelector('#player');
+const previous = document.querySelector('#previous');
+const shuffle = document.querySelector('#shuffle');
+const next = document.querySelector('#next');
+
+const tags = document.querySelector('#tags');
+
+const state = {
+    collection: null,
+    index: null,
+};
 
 function validateRedgifsUrl(url) {
     if (!url) {
@@ -24,6 +34,50 @@ function validateRedgifsUrl(url) {
     } catch (error) {
 	return false;
     }
+}
+
+function playVideo(index) {
+    const video = state.collection.content[state.index];
+
+    player.src = video.sources.hd;
+    player.play();
+
+    tags.innerHTML = '';
+
+    video.tags.forEach((tag) => {
+	const tagListItem = document.createElement('li');
+
+	tagListItem.textContent = tag;
+	tagListItem.classList.add('tag');
+
+	tags.appendChild(tagListItem);
+    });
+}
+
+function playRandomVideo() {
+    state.index = Math.floor(Math.random() * state.collection.content.length);
+
+    playVideo();
+}
+
+function playPreviousVideo() {
+    if (state.index > 0) {
+	state.index -= 1;
+    } else {
+	state.index = state.collection.content.length - 1;
+    }
+
+    playVideo();
+}
+
+function playNextVideo() {
+    if (state.index < state.collection.content.length - 1) {
+	state.index += 1;
+    } else {
+	state.index = 0;
+    }
+
+    playVideo();
 }
 
 async function retrieveCollection() {
@@ -46,6 +100,8 @@ async function retrieveCollection() {
     collectionTitle.textContent = `${collection.title} by ${collection.username}`;
     collectionSize.textContent = `${collection.content.length} GIFs`;
 
+    collectionContent.innerHTML = '';
+
     collection.content.forEach((gif) => {
 	const gifListItem = document.createElement('li');
 
@@ -54,9 +110,15 @@ async function retrieveCollection() {
 	collectionContent.appendChild(gifListItem);
     });
 
-    player.src = collection.content[Math.floor(Math.random() * collection.content.length)].sources.hd;
+    state.collection = collection;
 
     collectionInfo.classList.remove('hidden');
+
+    playRandomVideo();
 }
 
 redgifsLoad.addEventListener('click', retrieveCollection);
+
+previous.addEventListener('click', playPreviousVideo);
+shuffle.addEventListener('click', playRandomVideo);
+next.addEventListener('click', playNextVideo);
