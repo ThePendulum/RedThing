cde14acb9d81b8721f9115ab8cd85fac622e5511
Niels Simenon <niels.simenon@gmail.com>
2022-05-23 02:09:31 +0200

    Listening for PORT environment variable.

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ async function initServer() {
 
     app.use(router);
 
-    const server = app.listen(3000, () => {
+    const server = app.listen(process.env.PORT || 3000, () => {
 	const { address, port } = server.address();
 
 	logger.info(`Web server listening on ${address}:${port}`);
